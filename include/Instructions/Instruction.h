e19f2cbb40845432c2f2368c9253cc31be7462df
Rikka <2560359315@qq.com>
2025-07-15 01:36:42 +0800

    refactor: update Instruction class to use unique_ptr for MachineOperand management

--- include/Instructions/Instruction.h
+++ include/Instructions/Instruction.h
@@ -2,6 +2,8 @@
 
 // #include "BasicBlock.h"
 #include <list>
+#include <memory>
+#include <string>
 #include <vector>
 
 #include "MachineOperand.h"
@@ -41,23 +43,33 @@ class Instruction {
         : opcode(op), parent(parent) {}
 
     // 添加操作数
-    void addOperand(MachineOperand* operand) { operands.push_back(operand); }
+    void addOperand(std::unique_ptr<MachineOperand> operand) {
+        operands.push_back(std::move(operand));
+    }
 
     Opcode getOpcode() const { return opcode; }
-    const std::vector<MachineOperand*>& getOperands() const { return operands; }
+    const std::vector<std::unique_ptr<MachineOperand>>& getOperands() const {
+        return operands;
+    }
+    const auto getOprandCount() const { return operands.size(); }
 
     // 为了方便，可以提供一些辅助函数
     // 例如：获取第 n 个操作数
-    MachineOperand* getOperand(std::size_t n) const { return operands[n]; }
+    MachineOperand* getOperand(std::size_t n) const {
+        return operands[n].get();
+    }
 
     BasicBlock* getParent() const { return parent; }
     void setParent(BasicBlock* bb) { parent = bb; }
 
+    // 生成文本表示
+    std::string toString() const;
+
    private:
     Opcode opcode;
-    std::vector<MachineOperand*> operands;
+    std::vector<std::unique_ptr<MachineOperand>> operands;
 
-    // 指向其所在的基本块 (可选，但非常有用)
+    // 指向其所在的基本块 (可选，但非常有用) - 弱引用
     BasicBlock* parent{};
 };
 
