0da0dfecfbbe1ddf98f2f9aec8ce42e22d30c9c1
Rikka <2560359315@qq.com>
2025-07-15 21:50:44 +0800

    refactor: update signature in `Target`

--- src/Target.cpp
+++ src/Target.cpp
@@ -1,27 +1,45 @@
 #include "Target.h"
 #include "CodeGen.h"
 #include "IR/Function.h"
+#include "Visit.h"
 
 namespace riscv64 {
 
-std::vector<std::string> RISCV64Target::compileToAssembly(const midend::Module& module) {
+std::string RISCV64Target::compileToAssembly(const midend::Module& module) {
     std::vector<std::string> assembly;
-    CodeGenerator codegen;
+    // CodeGenerator codegen;
     
     // 添加汇编头部
-    assembly.push_back(".text");
-    assembly.push_back(".global _start");
+    assembly.emplace_back(".text");
+    assembly.emplace_back(".global _start");
     
     // 为每个函数生成代码
-    for (const auto* func : module) {
-        assembly.push_back("");
-        assembly.push_back(func->getName() + ":");
+    // for (const auto* func : module) {
+    //     assembly.emplace_back("");
+    //     assembly.push_back(func->getName() + ":");
         
-        auto funcCode = codegen.generateFunction(func);
-        assembly.insert(assembly.end(), funcCode.begin(), funcCode.end());
-    }
+    //     auto funcCode = codegen.generateFunction(func);
+    //     assembly.insert(assembly.end(), funcCode.begin(), funcCode.end());
+    // }
+    auto riscv_module = instructionSelectionPass(module);
     
-    return assembly;
+    
+    return riscv_module.toString();
+}
+
+Module RISCV64Target::instructionSelectionPass(const midend::Module& module) {
+    // 创建一个访客
+    CodeGenerator codegen;
+    // 使用访客访问模块
+    auto riscv_module = codegen.visitor_->visit(&module);
+
+    return riscv_module;
+}
+
+Module RISCV64Target::registerAllocationPass(riscv64::Module& module) {
+    // 这里可以实现寄存器分配逻辑
+    // 目前只是返回原始模块
+    return std::move(module);
 }
 
 }  // namespace riscv64
