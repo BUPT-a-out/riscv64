f44943a377ba3f9b5c67023d2f577cadda299d0d
Rikka <2560359315@qq.com>
2025-07-17 12:03:06 +0800

    WIP: add passes

--- include/Target.h
+++ include/Target.h
@@ -15,7 +15,11 @@ class RISCV64Target {
     std::string compileToAssembly(const midend::Module& module);
     Module instructionSelectionPass(
         const midend::Module& module);  // 指令选择，生成的都是虚拟寄存器
-    Module registerAllocationPass(
+    Module& reorderInstructionsPass(
+        riscv64::Module& module);  // 指令重排序，优化指令顺序
+    Module& basicBlockSchedulingPass(
+        riscv64::Module& module);  // 基本块调度，优化基本块顺序，删除无用的跳转
+    Module& registerAllocationPass(
         riscv64::Module& module);  // 寄存器分配，把虚拟寄存器分配到物理寄存器上
 };
 
