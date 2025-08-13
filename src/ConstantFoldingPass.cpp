789ef78bb7f9fe4f23d923ce50e1bd368fbbe044
Rikka <2560359315@qq.com>
2025-08-14 01:09:24 +0800

    feat: implement constant folding logic in ConstantFoldingPass

--- src/ConstantFoldingPass.cpp
+++ src/ConstantFoldingPass.cpp
@@ -1,9 +1,91 @@
 #include "ConstantFoldingPass.h"
 
+#include "Visit.h"
+
 namespace riscv64 {
 
 void ConstantFolding::runOnFunction(Function* function) {
     // Perform constant folding on the given function
+}
+
+void ConstantFolding::runOnBasicBlock(BasicBlock* basicBlock) {
+    // Perform constant folding on the given basic block
+    // init
+    virtualRegisterConstants.clear();
+    // instructionsToRemove.clear();
+
+    for (auto& inst : *basicBlock) {
+        handleInstruction(inst.get(), basicBlock);
+    }
+}
+
+void ConstantFolding::handleInstruction(Instruction* inst,
+                                        BasicBlock* parent_bb) {
+    // 处理重定义
+    if (inst->getOprandCount() >= 1) {
+        auto* defined_operand = inst->getOperand(0);
+        if (defined_operand->isReg()) {
+            // 旧值失效
+            virtualRegisterConstants.erase(defined_operand->getRegNum());
+        }
+    }
+
+    // 尝试折叠
+    foldInstruction(inst, parent_bb);
+    // 尝试窥孔优化
+    peepholeOptimize(inst, parent_bb);
+    // 尝试常量传播
+    constantPropagate(inst, parent_bb);
+}
+
+void ConstantFolding::foldInstruction(Instruction* inst,
+                                      BasicBlock* parent_bb) {
+    std::vector<int64_t> source_constants;
+    for (const auto& operand : inst->getOperands()) {
+        if (operand->isImm()) {
+            source_constants.push_back(operand->getValue());
+        } else if (operand->isReg()) {
+            auto it = virtualRegisterConstants.find(operand->getRegNum());
+            if (it != virtualRegisterConstants.end()) {
+                source_constants.push_back(it->second);
+            } else {
+                return;  // 无法折叠
+            }
+        } else {
+            return;  // 不支持的操作数类型
+        }
+    }
+
+    // 那么，可以被折叠
+    auto result =
+        calculateInstructionValue(inst->getOpcode(), source_constants);
+    if (!result.has_value()) {
+        return;  // 算不出来
+    }
+
+    auto* dest_reg = inst->getOperand(0);
+    virtualRegisterConstants[dest_reg->getRegNum()] = result.value();
+
+    // 替换指令
+    auto inst_backup = *inst;
+    inst->clearOperands();
+    inst->setOpcode(Opcode::LI);
+    inst->addOperand(std::make_unique<ImmediateOperand>(result.value()));
+
+    std::cout << "Folded instruction: '" << inst_backup.toString() << "' to '"
+              << inst->toString() << "'" << std::endl;
+}
+
+void ConstantFolding::peepholeOptimize(Instruction* inst,
+                                       BasicBlock* parent_bb) {}
+
+void ConstantFolding::constantPropagate(Instruction* inst,
+                                        BasicBlock* parent_bb) {}
+
+std::optional<int64_t> ConstantFolding::calculateInstructionValue(
+    Opcode op, std::vector<int64_t>& source_operands) {
+    // TODO(rikka): impl
+    return 114514;
 }
 
 }  // namespace riscv64
